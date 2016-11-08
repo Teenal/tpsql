68fa5ce0b62b4fad332ca1eab7ba31d2634d239d
gpyh <yacine.hmito@gmail.com>
2016-11-08 16:03:34 +0100

    Data qualification TP5

---
+++ tp5/requetes.sql
@@ -0,0 +1,5 @@
+INSERT INTO QUENTIN_Qualification ("Développeur",  500)
+INSERT INTO QUENTIN_Qualification ("Analyste",  700)
+INSERT INTO QUENTIN_Qualification ("Chef de projet",  900)
+INSERT INTO QUENTIN_Qualification ("Consultant", 1000)
+INSERT INTO QUENTIN_Qualification ("Directeur commercial", 1300)
